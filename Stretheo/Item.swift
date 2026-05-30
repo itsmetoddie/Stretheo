ac5f2f188d0aeab7e3a9e6e03c8523017b2018b4
zapreff <todor.zapreff@gmail.com>
2026-05-30 18:32:37 +0300

    Initial Commit

---
+++ Stretheo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Stretheo
+//
+//  Created by Todor Zapreff on 30.05.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StretheoTests/StretheoTests.swift
@@ -0,0 +1,19 @@
+//
+//  StretheoTests.swift
+//  StretheoTests
+//
+//  Created by Todor Zapreff on 30.05.26.
+//
+
+import Testing
+@testable import Stretheo
+
+struct StretheoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
